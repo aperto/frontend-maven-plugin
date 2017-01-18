53b26e0d586cb8e8c164bc568449497dafe76c3a
Jörg von Frantzius <joerg.frantzius@aperto.com>
2017-01-18 16:53:58 +0100

    npm dependencies extraction: finish implementation

--- frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/NpmDependenciesMojo.java
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/NpmDependenciesMojo.java
@@ -4,19 +4,42 @@ import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
 import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
 import com.github.eirslett.maven.plugins.frontend.lib.TaskRunnerException;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.model.Build;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.apache.wink.json4j.JSONException;
 import org.apache.wink.json4j.OrderedJSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Extract given npm packages and their dependencies from node_modules and copy them to given destination
  * directory in target.
  */
+@Mojo(name = "extractDependencies", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
 public class NpmDependenciesMojo extends AbstractNpmMojo {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());;
+
     private int lineCounter = 0;
+
+    @Parameter(readonly = true, defaultValue = "${project}")
+    private MavenProject project;
 
     /**
      * npm packages to extract dependencies from.
@@ -24,8 +47,56 @@ public class NpmDependenciesMojo extends AbstractNpmMojo {
     @Parameter(property = "frontend.npmDependencies.npmPackages", required = true)
     protected String[] npmPackages;
 
+    @Parameter(property = "frontend.npmDependencies.targetDir", required = true)
+    protected String targetDir;
+
+    /**
+     * Reduce to top-level folders within topmost node_modules folder
+     */
+    protected Set<String> flattenToToplevelFolders(List<String> directories) {
+        Set<String> result = new TreeSet<>();
+
+        String[] arr = new String[directories.size()];
+        directories.toArray(arr);
+
+        String commonPrefix = StringUtils.getCommonPrefix(arr);
+
+        for (String folder : directories) {
+            int separatorAfterName = folder.indexOf(File.separator, commonPrefix.length());
+            String topLevelFolder;
+            if (separatorAfterName==-1) {
+                topLevelFolder = folder.substring(commonPrefix.length());
+            } else {
+                topLevelFolder = folder.substring(commonPrefix.length(), separatorAfterName);
+            }
+            result.add(commonPrefix + topLevelFolder);
+        }
+        return result;
+    }
+
     protected void copyDependencies(String fullDirectoryList, int startline, int numLines) {
-        // TODO copy directories
+        Build build = project.getModel().getBuild();
+
+        BufferedReader bufReader = new BufferedReader(new StringReader(fullDirectoryList));
+        List<String> lines = new ArrayList<>(10000);
+        String line = null;
+        try {
+            while ((line = bufReader.readLine()) != null) {
+                lines.add(line);
+            }
+            List<String> directories = lines.subList(startline, startline + numLines + 1);
+
+            Set<String> topLevelFolders = flattenToToplevelFolders(directories);
+            for (String directory : topLevelFolders) {
+                File from = new File(directory);
+                File to = Paths.get(build.getDirectory(), targetDir, from.getName()).toFile();
+                logger.info("extractDependencies: copying " + from + " to " + to);
+                org.apache.maven.shared.utils.io.FileUtils.copyDirectoryStructure(from, to);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private OrderedJSONObject depthFirstSearchDependency(OrderedJSONObject root, String npmPackage) {
@@ -55,19 +126,18 @@ public class NpmDependenciesMojo extends AbstractNpmMojo {
 
     @Override
     protected void executeNpm(FrontendPluginFactory factory, ProxyConfig proxyConfig) throws TaskRunnerException {
-        String arguments = "get config json";
-        String restoreConfigJson = factory.getNpmRunner(proxyConfig, getRegistryUrl()).executeWithResult(arguments, environmentVariables);
-
         // obtain package tree as json
         String packageTreeJson = getPackageTreeJson(factory, proxyConfig);
 
         String directoryList = getPackageDirectoryList(factory, proxyConfig);
 
+        logger.info("extractDependencies: copying npm packages " + Arrays.toString(npmPackages) + " to " + targetDir);
         for (String npmPackageName : npmPackages) {
             lineCounter = 0;
             OrderedJSONObject root;
             try {
-                // must use OrderedJSONObject so order in packageTreeJson DFS traversal will be same as in directoryList
+                // must use OrderedJSONObject so order in packageTreeJson DFS
+                // traversal will be same as in directoryList
                 root = new OrderedJSONObject(packageTreeJson);
             } catch (JSONException e) {
                 throw new RuntimeException(e);
@@ -83,20 +153,15 @@ public class NpmDependenciesMojo extends AbstractNpmMojo {
             int numLines = lineCounter;
             copyDependencies(directoryList, startLine, numLines);
         }
-
-        // restoreConfigJson
-        factory.getNpmRunner(proxyConfig, getRegistryUrl()).execute("set config json " + restoreConfigJson, environmentVariables);
     }
 
     protected String getPackageDirectoryList(FrontendPluginFactory factory, ProxyConfig proxyConfig) throws TaskRunnerException {
-        factory.getNpmRunner(proxyConfig, getRegistryUrl()).execute("set config json false", environmentVariables);
-        String directoryList = factory.getNpmRunner(proxyConfig, getRegistryUrl()).executeWithResult("ls", environmentVariables);
+        String directoryList = factory.getNpmRunner(proxyConfig, getRegistryUrl()).executeWithResult("ls --parseable true --json false", environmentVariables);
         return directoryList;
     }
 
     protected String getPackageTreeJson(FrontendPluginFactory factory, ProxyConfig proxyConfig) throws TaskRunnerException {
-        factory.getNpmRunner(proxyConfig, getRegistryUrl()).execute("set config json true", environmentVariables);
-        String packageTreeJson = factory.getNpmRunner(proxyConfig, getRegistryUrl()).executeWithResult("ls", environmentVariables);
+        String packageTreeJson = factory.getNpmRunner(proxyConfig, getRegistryUrl()).executeWithResult("ls --parseable false --json true", environmentVariables);
         return packageTreeJson;
     }
 
